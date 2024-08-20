4575fe8a0acbcbe1b7474410e20aa463696831c0
frwd-1 <frwdlabs@gmail.com>
2024-08-20 17:32:25 -0600

    wip

--- src/utils/chain_utils.rs
+++
@@ -1,9 +0,0 @@
-use reth_primitives::TransactionSigned;
-use reth_provider::Chain;
-
-pub fn decode_chain_into_transactions(chain: &Chain) -> impl Iterator<Item = &TransactionSigned> {
-    println!("Decoding chain into transactions");
-    chain
-        .blocks_iter()
-        .flat_map(|block_with_senders| block_with_senders.body.iter())
-}
